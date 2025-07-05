69a690ffc9040bcf00f03f32cd0a54f5c34cb3d2
lokesh-saragadam <lokeshs2k6@gmail.com>
2025-07-05 19:54:36 +0530

    comparision

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ with st.expander("📚 What I Learned During This Project"):
     """)
 
 
-plot_comparison(loaded_history1, "Adadelta", loaded_history2, "Adam")        
+st.image('Screenshot 2025-07-05 192337.png')
